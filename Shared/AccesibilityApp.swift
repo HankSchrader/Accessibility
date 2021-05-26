47e57fb1fdaa03a3b8431aa11a9e5b137ef0201f
Erik Mikac <mikacew@gmail.com>
2021-05-26 07:25:17 -0400

    Initial Commit

---
+++ Shared/AccesibilityApp.swift
@@ -0,0 +1,17 @@
+//
+//  AccesibilityApp.swift
+//  Shared
+//
+//  Created by Erik Mikac on 5/26/21.
+//
+
+import SwiftUI
+
+@main
+struct AccesibilityApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
